3c483f41a009d995c6ceb9560396d1ad2ceac32c
Eduardo Zimermam <eduardozimerman@live.com>
2017-03-16 16:42:17 -0300

    Insertion of the function for fetch in the program

--- BST_Function.c
+++ BST_Function.c
@@ -6,12 +6,12 @@ typedef struct node_t {
 	struct node_t *left, *right, *father;			//Struct to create the node type.			
 } node;
 
-node create_node(int term){
+node *create_node(int term){
 
 	node *first;
 
-	first = (node*) malloc (sizeof(node*));						//Function to create the first node of the program.
-	first->key = term;
+	first = (node*) malloc (sizeof(node*));						
+	first->key = term;								//Function to create the first node of the program.
 	first->left = NULL;
 	first->right = NULL;
 	first->father = NULL;
@@ -19,14 +19,25 @@ node create_node(int term){
 	return(first);
 }
 
-node insert(node *node, int term){
+node *insert(node *node, int term){
 
 	if (node == NULL)
 		return(create_node(term));
 	if (term < node->key)
-		node->left = insert(node->left, term)
+		node->left = insert(node->left, term)					//Funtion for insertion of a node in BST.
 	else
 		node->right = insert(node->right, term);
 
 	return(node);
 }
+
+ node *search (node *node, int term){
+
+ 	while (node != NULL) && (term != node->key)
+ 		if (term < node->key)
+ 			node = node->left									//Funtion for fetch a node in BST.
+ 		else
+ 			node = node->right;
+
+ 		return(node);
+ }
