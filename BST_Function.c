19edc732b1b61cab03eff19e1ae420f5c5aa55a0
Eduardo Zimermam <eduardozimerman@live.com>
2017-03-16 16:23:23 -0300

    Insert the function insert

--- BST_Function.c
+++ BST_Function.c
@@ -2,16 +2,31 @@
 #include <stdlib.h>
 
 typedef struct node_t {
-	char *key;
+	int key;
 	struct node_t *left, *right, *father;			//Struct to create the node type.			
 } node;
 
-void create_node(){
+node create_node(int term){
 
 	node *first;
 
-	first = (node*) malloc (sizeof(node*));			//Function to create the first node of the program.
+	first = (node*) malloc (sizeof(node*));						//Function to create the first node of the program.
+	first->key = term;
 	first->left = NULL;
 	first->right = NULL;
 	first->father = NULL;
+
+	return(first);
+}
+
+node insert(node *node, int term){
+
+	if (node == NULL)
+		return(create_node(term));
+	if (term < node->key)
+		node->left = insert(node->left, term)
+	else
+		node->right = insert(node->right, term);
+
+	return(node);
 }
